3fec859d781c4176df96ec9c533e18b6a366d5da
Anthony Cajacuri <ancaors1@gmail.com>
2018-12-10 11:44:08 -0500

    modificaciones pequeñas

--- src/app/student-observable/student-observable.component.ts
+++ src/app/student-observable/student-observable.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map, reduce } from 'rxjs/operators'
 
 @Component({
   selector: 'app-student-observable',
@@ -7,6 +8,10 @@ import { Observable } from 'rxjs';
   styleUrls: ['./student-observable.component.css']
 })
 export class StudentObservableComponent implements OnInit {
+  item: any = [];
+  totalObs(totalObs: any): any {
+    throw new Error("Method not implemented.");
+  }
   students_obs;
   students = [
     {id: 1, name: 'cris'},
@@ -16,24 +21,64 @@ export class StudentObservableComponent implements OnInit {
   total: number;
   alumnosTotal: number;
   id: number = 3;
-  studentsObservable: any;
+  studentsObservable: Observable<any>;
+  studentsTotal;
+
 
   constructor() { 
-    this.studentsObservable = this.getStudents();
-    this.studentsObservable.subscribe((studentsData) => {
-      this.students_obs = studentsData;
-    });
+   
+    //   map( item => {
+    //     return item.id
+    //   })
+    // ).subscribe(res=> {
+    //   console.log(res)
+    //   this.students_obs = res;
+    //   console.log(this.studentsObservable)
+    // });
+    // this.studentsTotal = this.getTotalStudents();
+    // this.studentsTotal.subscribe( (st) => {
+    //   this.total = st
+    // })
+    console.log(this.studentsObservable)
 
-    this.calcularTotal();   
-    this.calcularAlumnos();
+    // console.log('item');
+    // console.log(this.item);
+    // this.totalObs = this.getStudents().subscribe().pipe(
+    //   map( item => item )
+    // )
+    
+    // console.log(this.totalObs);
+    // this.calcularTotal();   
+    // this.calcularAlumnos();
   }
 
   ngOnInit() {
+    this.studentsObservable = this.getStudents();
+    this.studentsObservable.subscribe((studentsData) => {
+      this.students_obs = studentsData;
+      console.log('entrar')
+      this.calcularTotal();   
+      this.calcularAlumnos(); 
+      
+    });
+    
   }
 
-  getStudents(): any {
+  getStudents(): Observable<any> {
     const studentsObservable = new Observable(observer => {
       observer.next(this.students);
+    });
+
+     return studentsObservable;
+  }
+
+  getTotalStudents(): any {
+    const studentsObservable = new Observable(observer => {
+      observer.next(
+        this.students
+          .map( item => item.id)
+          .reduce( (accum, val) => accum + val)
+      );
     });
 
      return studentsObservable;
@@ -43,6 +88,10 @@ export class StudentObservableComponent implements OnInit {
     this.total = this.students_obs
     .map( (item) => item.id)
     .reduce( (acc,val) => acc + val,0);
+    // .pipe(
+    //   map( (item) => item ),
+    //   reduce( (acc, val) => acc + val, 0)
+    // )
   }
 
   calcularAlumnos() {
@@ -56,8 +105,14 @@ export class StudentObservableComponent implements OnInit {
       id: this.id,
       name: 'cristian '+this.id
     })
+    // console.log(this.totalObs);
     this.calcularTotal();  
     this.calcularAlumnos();
+  }
+
+  printer() {
+    // console.log(this.item)
+    // console.log(this.alumnosTotal)
   }
 
   eliminarFinal() {
